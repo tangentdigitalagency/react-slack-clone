e0157421ccc7d33ce74f1147a09053dbe7a8a61a
David <david@tangentdigitalagency.com>
2021-05-05 18:50:04 -0700

    Made Login page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import {Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react'; 
+import { Link } from 'react-router-dom';
 
 class Register extends React.Component {
+
+    //Handle Change For form
+    state = {}
+
+    handleChange = () => {}
 
     render(){
 
         return(
-            <div>
-                Register
-            </div>
+           <Grid textAlign="center" verticalAlign="middle" className="app">
+               <Grid.Column style={{maxWidth: 450}}>
+                    <Header as="h2" icon color="black" textAlign="center">
+                        <Icon name="puzzle piece" color="black" />
+                            Register For Slack
+                    </Header>
+
+                    <Form size="large">
+                        <Segment stacked>
+                            <Form.Input fluid name="User Name" icon="user" iconPosition="left" placeholder="Enter Your Username" onchange={this.handleChange} type="text" />
+
+                            <Form.Input fluid name="Email Address" icon="mail" iconPosition="left" placeholder="Enter Your Email" onchange={this.handleChange} type="email" />
+
+                            <Form.Input fluid name="Password" icon="lock" iconPosition="left" placeholder="Enter Your Password" onchange={this.handleChange} type="password" />
+
+                            <Form.Input fluid name="Repeart Password" icon="repeat" iconPosition="left" placeholder="Re-Enter Your Password" onchange={this.handleChange} type="password" />
+
+                            <Button color="black"> Submit </Button>
+                        </Segment>
+                    </Form>
+                
+                    <Message> Already A User? <Link to="/login"> Login Here!</Link></Message>
+               </Grid.Column>
+           </Grid>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import 'semantic-ui-css/semantic.min.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
